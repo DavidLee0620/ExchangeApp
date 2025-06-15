75cb1a0110308953f01d0ffebbef4c9ace756c4d
Lee <759859067@qq.com>
2025-06-15 17:00:05 +0800

    feat-引入gorm

---
+++ Exchangeapp_backend/config/db.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"log"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func initDB() {
+	dsn := AppConfig.Database.Dsn
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("gorm.Open is err:%v", err)
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("db.DB is err:%v", err)
+	}
+	sqlDB.SetConnMaxIdleTime(time.Duration(AppConfig.Database.MaxIdleConns))
+	sqlDB.SetMaxOpenConns(AppConfig.Database.MaxOpenConns)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+}
