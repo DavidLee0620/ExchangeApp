64d385f0ca841674caea045d82fc3cd8e363bc96
Lee <759859067@qq.com>
2025-06-20 21:32:16 +0800

    feat-添加redis缓存部分

--- Exchangeapp_backend/controllers/artical_controller.go
+++ Exchangeapp_backend/controllers/artical_controller.go
@@ -1,14 +1,19 @@
 package controllers
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/DavidLee0620/ExchangeApp/Exchangeapp_backend/global"
 	"github.com/DavidLee0620/ExchangeApp/Exchangeapp_backend/model"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis"
 	"gorm.io/gorm"
 )
+
+var cacheKey = "articals"
 
 func CreateArtical(ctx *gin.Context) {
 	var artical model.Artical
@@ -24,20 +29,48 @@ func CreateArtical(ctx *gin.Context) {
 		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+	if err := global.RedisDB.Del(cacheKey).Err(); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 	ctx.JSON(http.StatusCreated, artical)
 }
 
 func GetArtical(ctx *gin.Context) {
-	var articals []model.Artical
-	if err := global.DB.Find(&articals).Error; err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			ctx.JSON(http.StatusNotFound, gin.H{"err": err.Error()})
-		} else {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
+	cacheData, err := global.RedisDB.Get(cacheKey).Result()
+	if err == redis.Nil {
+		var articals []model.Artical
+		if err := global.DB.Find(&articals).Error; err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				ctx.JSON(http.StatusNotFound, gin.H{"err": err.Error()})
+			} else {
+				ctx.JSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
+			}
+			return
 		}
+		articalJson, err := json.Marshal(articals)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		if err := global.RedisDB.Set(cacheKey, articalJson, 10*time.Minute).Err(); err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		ctx.JSON(http.StatusOK, articals)
+	} else if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
+	} else {
+		var artical []model.Artical
+		if err := json.Unmarshal([]byte(cacheData), &artical); err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, artical)
 	}
-	ctx.JSON(http.StatusOK, articals)
+
 }
 
 func GetArticalByID(ctx *gin.Context) {
@@ -51,6 +84,7 @@ func GetArticalByID(ctx *gin.Context) {
 		}
 		return
 	}
+
 	ctx.JSON(http.StatusOK, artical)
 
 }
