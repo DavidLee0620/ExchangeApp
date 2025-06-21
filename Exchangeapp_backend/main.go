6d876eb49df9ff00f33bd608f8e832e5f7dcae39
Lee <759859067@qq.com>
2025-06-21 12:01:58 +0800

    feat-添加优雅的退出程序

--- Exchangeapp_backend/main.go
+++ Exchangeapp_backend/main.go
@@ -1,6 +1,13 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
 	"github.com/DavidLee0620/ExchangeApp/Exchangeapp_backend/config"
 	"github.com/DavidLee0620/ExchangeApp/Exchangeapp_backend/route"
 )
@@ -9,5 +16,26 @@ func main() {
 	config.InitConfig()
 	r := route.SetupRouter()
 
-	r.Run(config.AppConfig.App.Port) // listen and serve
+	srv := &http.Server{
+		Addr:    config.AppConfig.App.Port,
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
