0470a545e51e24084e02582473fc660433a28062
Lee <759859067@qq.com>
2025-06-21 10:17:04 +0800

    fix-debug跨域请求的http

--- Exchangeapp_backend/route/route.go
+++ Exchangeapp_backend/route/route.go
@@ -1,13 +1,24 @@
 package route
 
 import (
+	"time"
+
 	"github.com/DavidLee0620/ExchangeApp/Exchangeapp_backend/controllers"
 	"github.com/DavidLee0620/ExchangeApp/Exchangeapp_backend/middleware"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true, //是否允许发送cookie和证书
+		MaxAge:           12 * time.Hour,
+	}))
 	auth := r.Group("/api/auth")
 	{
 		auth.POST("/login", controllers.Login)
